2599414c4895503e303a936746f4e30723bed617
潘嘉华 <scyjp2@nottingham.edu.cn>
2021-10-13 16:49:43 +0800

    update web-basics-practice

--- docs/content/examples/web-practice-1.js
+++ docs/content/examples/web-practice-1.js
@@ -2,7 +2,7 @@ const HEIGHT = window.innerHeight;
 const WIDTH = window.innerWidth;
 
 function move(){
-	var button = document.getElementById('huaji');
+	var button = document.getElementById('run');
 	var max_top = HEIGHT - 100;
 	var max_left = WIDTH - 100;
 	button.style.top = Math.round(Math.random()*max_top) + 'px';
--- docs/content/web-basics-practice.md
+++ docs/content/web-basics-practice.md
@@ -4,6 +4,8 @@
 ## 实例
 预览<a href="../examples/web-practice-1.html">页面</a>
 
+素材<a href="../examples/run.zip">下载</a>
+
 下载<a href="../examples/example-web-1.zip">页面</a>，解压后用浏览器和编辑器同时打开。
 
 把编辑器（代码）放在左边，网页放在右边。这样，每当你改变代码并保存，刷新网页就会自动更新。
